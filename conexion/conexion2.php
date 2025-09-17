425786673c3dd0c5612124b168668d359480912f
Wilmar Alastre <51450021+walastre64@users.noreply.github.com>
2025-09-17 15:17:07 -0400

    Update conexion2.php

--- conexion/conexion2.php
+++ conexion/conexion2.php
@@ -3,7 +3,7 @@ function conectate()
 {	
 	$Bd    = "10.50.10.25\MSSQLSERVER2012";
 	$Usu   = "sa";
-	$Clave = "T3mPoral00";
+	$Clave = "";
 
 	$link=mssql_connect($Bd,$Usu,$Clave ); //or die("0|Error conectando a la base de datos.");
 	mssql_select_db("Helpdesk",$link); //or die("0|Error seleccionando la base de datos.");
