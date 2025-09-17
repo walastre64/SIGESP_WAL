7625bda12da1fb929d18025234ce2f4075185a6c
Wilmar Alastre <51450021+walastre64@users.noreply.github.com>
2025-09-17 15:17:47 -0400

    Update conexionsqlsrv.php

--- conexion_wilmar/conexionsqlsrv.php
+++ conexion_wilmar/conexionsqlsrv.php
@@ -4,13 +4,13 @@ function conectate(){
 		$serverName = "A000462820"; //serverName\instanceName
 		$connectionInfo = array( "Database"		=>"AESIGESP",
 								 "UID"	   		=>"sa",
-								 "PWD"	   		=>"T3mP0ral77",
+								 "PWD"	   		=>"",
 								 "CharacterSet" =>"UTF-8"
 								 );
 		$conn = sqlsrv_connect( $serverName, $connectionInfo);
 	
 		if( $conn  == false) {
-			 echo "Conexión no se pudo establecer.<br />";
+			 echo "ConexiÃ³n no se pudo establecer.<br />";
 			 die( print_r( sqlsrv_errors(), true));
 		}else{
 			return $conn;
